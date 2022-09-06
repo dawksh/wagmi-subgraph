2a9d82dca09703c062388d4f76d903ed8028c357
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-09-06 17:37:47 +0530

    init

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,49 +1,26 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Wagmi, AgreementAdded, Signed } from "../generated/Wagmi/Wagmi"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Wagmi, AgreementAdded, Signed } from "../generated/Wagmi/Wagmi";
+import { AgreementEntity } from "../generated/schema";
 
 export function handleAgreementAdded(event: AgreementAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+	// Entities can be loaded from the store using a string ID; this ID
+	// needs to be unique across all entities of the same type
+	let entity = AgreementEntity.load(event.transaction.from.toHex());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.agreement = event.params.agreement
-  entity.signer = event.params.signer
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.indexToAgreementStorage(...)
-  // - contract.signerToAgreementMapping(...)
-  // - contract.verifyAgreement(...)
+	// Entities only exist after they have been saved to the store;
+	// `null` checks allow to create entities on demand
+	if (!entity) {
+		entity = new AgreementEntity(`${event.params.id}`);
+		entity.agreement = event.params.agreement;
+		entity.count = event.params.id;
+		entity.signer = event.params.signer;
+		entity.isSigned = false;
+	}
 }
 
-export function handleSigned(event: Signed): void {}
+export function handleSigned(event: Signed): void {
+	let entity = AgreementEntity.load(event.params.id.toString());
+	if (entity) {
+		entity.isSigned = true;
+	}
+}
